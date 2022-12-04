a63a085de1b36f2759730f952fab6f5719bb48cf
Mikhail Nikitin <nitinin.miha@icloud.com>
2022-12-04 12:13:49 +0700

    #8
    Initiated service

---
+++ common-api-category/src/main/java/com/gb/financial/assistant/commonapicategory/CommonApiCategoryApplication.java
@@ -0,0 +1,13 @@
+package com.gb.financial.assistant.commonapicategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonApiCategoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApiCategoryApplication.class, args);
+    }
+
+}
